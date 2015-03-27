3111267660e31435e2ee0662259614489fbb39e9
Ezequiel <Ezequiel@EzequielPC>
2015-03-27 13:32:17 -0300

    Subida inicial

---
+++ desapp-groupa-backend/src/main/java/ar/edu/desapp/grupoa/App.java
@@ -0,0 +1,13 @@
+package ar.edu.desapp.grupoa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
