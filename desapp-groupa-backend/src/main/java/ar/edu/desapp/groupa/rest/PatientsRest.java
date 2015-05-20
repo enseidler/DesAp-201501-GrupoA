efd309564248daeeb5fa4c0841b975d47c03a301
Guillermo <guillens91@gmail.com>
2015-05-19 21:29:14 -0300

    Rest CreatePatient

--- desapp-groupa-backend/src/main/java/ar/edu/desapp/groupa/rest/PatientsRest.java
+++ desapp-groupa-backend/src/main/java/ar/edu/desapp/groupa/rest/PatientsRest.java
@@ -2,8 +2,12 @@ package ar.edu.desapp.groupa.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import ar.edu.desapp.groupa.model.user.Patient;
@@ -19,11 +23,23 @@ public class PatientsRest {
 	}
 
 	@GET
-    @Path("/all")
+    @Path("/list")
     @Produces("application/json")
-    public List<Patient> retriveAll() {
+    public List<Patient> getPatients() {
         List<Patient> patients = patientService.retriveAll();
         return patients;
     }
+	
+	@POST
+	@Path("/create")
+	@Consumes("application/x-www-form-urlencoded")
+	public void createPatient(@FormParam("name") String name, 
+								@FormParam("surname") String surname,
+								@FormParam("dni") Integer dni,
+								@FormParam("password") String password,
+								@FormParam("weight") double weight,
+								@FormParam("height") double height) {
+		patientService.save(new Patient(name, surname, dni, password, weight, height));
+	}
 	
 }
