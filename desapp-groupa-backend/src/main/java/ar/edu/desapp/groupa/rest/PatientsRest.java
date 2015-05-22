60e3c4ff0abd61a563f74c4e2c86b2c3f939d8f7
Ezequiel <enseidler@gmail.com>
2015-05-22 17:27:45 -0300

    Added PatientsRest Create, and web/pom settings changes

--- desapp-groupa-backend/src/main/java/ar/edu/desapp/groupa/rest/PatientsRest.java
+++ desapp-groupa-backend/src/main/java/ar/edu/desapp/groupa/rest/PatientsRest.java
@@ -1,15 +1,12 @@
 package ar.edu.desapp.groupa.rest;
 
 import java.util.List;
-
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
+import javax.ws.rs.core.Response;
 import ar.edu.desapp.groupa.model.user.Patient;
 import ar.edu.desapp.groupa.services.PatientService;
 
@@ -32,14 +29,11 @@ public class PatientsRest {
 	
 	@POST
 	@Path("/create")
-	@Consumes("application/x-www-form-urlencoded")
-	public void createPatient(@FormParam("name") String name, 
-								@FormParam("surname") String surname,
-								@FormParam("dni") Integer dni,
-								@FormParam("password") String password,
-								@FormParam("weight") double weight,
-								@FormParam("height") double height) {
-		patientService.save(new Patient(name, surname, dni, password, weight, height));
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response createPatient(Patient patient) {
+		patientService.save(patient);
+		return Response.ok(patient).build();
 	}
 	
 }
