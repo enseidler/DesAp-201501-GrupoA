09545b81f47b42f4e1807000dafd92534c9ac5f3
Ezequiel <enseidler@gmail.com>
2015-05-25 18:36:38 -0300

    Patient modification panel Started in progress

--- desapp-groupa-frontend/app/js/controllers.js
+++ desapp-groupa-frontend/app/js/controllers.js
@@ -11,11 +11,16 @@ app.config(['$httpProvider', '$routeProvider', function ($httpProvider, $routePr
         	templateUrl: 'views/create-patient.html',
         	controller: 'CreatePatientController'
       	}).
+      	when('/ModifyPatient', {
+        	templateUrl: 'views/modify-patient.html',
+        	controller: 'ModifyPatientController'
+      	}).
       	otherwise({
         	redirectTo: '/'
     	});
 
 }]);
+
 
 
 app.controller('CreatePatientController', ['$scope', '$http', '$resource', function($scope, $http, $resource) {
@@ -24,5 +29,10 @@ app.controller('CreatePatientController', ['$scope', '$http', '$resource', funct
 		var req = $resource('http://localhost:8080/desapp-groupa-backend/rest/patients/create');
 		req.save($scope.newPatient);
 	};
+	
+}]);
+
+
+app.controller('ModifyPatientController', ['$scope', '$http', function($scope, $http) {	
 	
 }]);
