83581012b2768f3674c6993e4d41f0fe9fde2bba
Ezequiel <enseidler@gmail.com>
2015-05-26 14:07:27 -0300

    Refactor main menu code

--- desapp-groupa-frontend/app/js/facemed-accordion.js
+++ desapp-groupa-frontend/app/js/facemed-accordion.js
@@ -1,24 +1,19 @@
 'use strict';
 
 /** 
-**	FACEMED ACCORDION MENU
+**	FACEMED ACCORDION
 **  Forked from: http://www.learningjquery.com/2007/03/accordion-madness
 **  Author: Ezequiel N. Seidler
 **  Universidad Nacional de Quilmes	
 */
 
 $(document).ready(function() {
-	$('div.facemed-accordion div.panel div.panel-body').hide();  
-	$('div.facemed-accordion div.panel div.panel-heading').click(function() {
-	  	var $nextDiv = $(this).next('div.panel-body');
-	  	var $visibleSiblings = $nextDiv.siblings('div.panel div.panel-body:visible');
-		  
-		if ($visibleSiblings.length ) {
-	    	$visibleSiblings.slideUp('fast', function() {
-	      		$nextDiv.slideToggle('fast');
-	    	});
-	  	} else {
-	     	$nextDiv.slideToggle('fast');
-	  	}
+	
+	$('div.facemed-accordion-body').hide();  
+	
+	$('div.facemed-accordion-option').click(function() {
+	  	var $nextDiv = $(this).next('div.facemed-accordion-body');
+		$nextDiv.slideToggle('fast');
 	});
+
 });
