22b49281aa47c4bdbbd0fe272a67911be93f8895
Ezequiel <enseidler@gmail.com>
2015-06-02 15:00:25 -0300

    Symptom rest service added and integrated to spring

---
+++ desapp-groupa-backend/src/main/java/ar/edu/desapp/groupa/rest/SymptomRest.java
@@ -0,0 +1,37 @@
+package ar.edu.desapp.groupa.rest;
+
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import ar.edu.desapp.groupa.model.medicalRecord.MedicalRecord;
+import ar.edu.desapp.groupa.model.symptom.Symptom;
+import ar.edu.desapp.groupa.services.MedicalRecordService;
+import ar.edu.desapp.groupa.services.SymptomService;
+
+
+@Path("/symptoms")
+public class SymptomRest {
+	
+	private SymptomService symptomService; 
+	
+	public void setSymptomService(final SymptomService symptomService) {
+		this.symptomService = symptomService;
+	}
+
+	@GET
+	@Path("/list")
+	@Produces("application/json")
+	public List<Symptom> list() {
+		List<Symptom> symptoms = symptomService.retriveAll();
+		return symptoms;
+	}
+	
+}
