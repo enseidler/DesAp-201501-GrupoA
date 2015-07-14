27e9f9c81df390a29b69215472deed83b01856c2
guillermo <guillermo.n.s@hotmail.com>
2015-07-14 12:59:48 -0300

    Create ErrorHAndler Aspect

---
+++ desapp-groupa-backend/src/main/java/ar/edu/desapp/groupa/aspects/ErrorHandlerService.java
@@ -0,0 +1,26 @@
+package ar.edu.desapp.groupa.aspects;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+public class ErrorHandlerService {
+
+	@Aspect
+	public class LoggerAspectExceptionsService {
+		Logger logger;
+		
+		@Around("execution( * ar.edu.unq.desapp.grupoa.services..*(..))")
+		public Object logException(ProceedingJoinPoint method) throws Throwable{
+			try {
+				return method.proceed();
+			} catch (Exception e) {
+				logger = Logger.getLogger(method.getTarget().getClass());
+				logger.error(e);
+				throw new Exception();
+			}
+		}
+	}
+
+}
--- desapp-groupa-backend/src/test/java/ar/edu/desapp/groupa/persistence/PersistabilityTest.java
+++ desapp-groupa-backend/src/test/java/ar/edu/desapp/groupa/persistence/PersistabilityTest.java
@@ -1,5 +1,10 @@
 package ar.edu.desapp.groupa.persistence;
 
+
+
+
 public class PersistabilityTest {
+	
 
 }
+
